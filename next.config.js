c314b787ef33d09e56e3afc504228db4adc0bf00
Dk2626 <52568067+Dk2626@users.noreply.github.com>
2021-08-14 21:14:47 +0530

    front

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiZGsyNiIsImEiOiJja3Nidjloa2UwYWpiMnZtY3lkaWpmeGlhIn0.BIHiqMgn0_bCFs9u9AtB5g",
+  },
 };
